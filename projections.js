5d294ab5092395d97ff2c91b68bb0b2bd7602970
Justin Obara <obara.justin@gmail.com>
2020-04-13 16:49:50 -0400

    Add projections data

---
+++ projections.js
@@ -0,0 +1,262 @@
+const PROJECTIONS = {
+	best: [{
+		"Reported Date": "2020-03-19T00:00:00",
+		"Number of patients in ICU with COVID-19": 0
+	}, {
+		"Reported Date": "2020-03-20T00:00:00",
+		"Number of patients in ICU with COVID-19": 0
+	}, {
+		"Reported Date": "2020-03-21T00:00:00",
+		"Number of patients in ICU with COVID-19": 0
+	}, {
+		"Reported Date": "2020-03-22T00:00:00",
+		"Number of patients in ICU with COVID-19": 8
+	}, {
+		"Reported Date": "2020-03-23T00:00:00",
+		"Number of patients in ICU with COVID-19": 10
+	}, {
+		"Reported Date": "2020-03-24T00:00:00",
+		"Number of patients in ICU with COVID-19": 20
+	}, {
+		"Reported Date": "2020-03-25T00:00:00",
+		"Number of patients in ICU with COVID-19": 30
+	}, {
+		"Reported Date": "2020-03-26T00:00:00",
+		"Number of patients in ICU with COVID-19": 40
+	}, {
+		"Reported Date": "2020-03-27T00:00:00",
+		"Number of patients in ICU with COVID-19": 59
+	}, {
+		"Reported Date": "2020-03-28T00:00:00",
+		"Number of patients in ICU with COVID-19": 74
+	}, {
+		"Reported Date": "2020-03-29T00:00:00",
+		"Number of patients in ICU with COVID-19": 108
+	}, {
+		"Reported Date": "2020-03-30T00:00:00",
+		"Number of patients in ICU with COVID-19": 140
+	}, {
+		"Reported Date": "2020-03-31T00:00:00",
+		"Number of patients in ICU with COVID-19": 200
+	}, {
+		"Reported Date": "2020-04-01T00:00:00",
+		"Number of patients in ICU with COVID-19": 245
+	}, {
+		"Reported Date": "2020-04-02T00:00:00",
+		"Number of patients in ICU with COVID-19": 320
+	}, {
+		"Reported Date": "2020-04-03T00:00:00",
+		"Number of patients in ICU with COVID-19": 419
+	}, {
+		"Reported Date": "2020-04-04T00:00:00",
+		"Number of patients in ICU with COVID-19": 540
+	}, {
+		"Reported Date": "2020-04-05T00:00:00",
+		"Number of patients in ICU with COVID-19": 710
+	}, {
+		"Reported Date": "2020-04-06T00:00:00",
+		"Number of patients in ICU with COVID-19": 750
+	}, {
+		"Reported Date": "2020-04-07T00:00:00",
+		"Number of patients in ICU with COVID-19": 800
+	}, {
+		"Reported Date": "2020-04-08T00:00:00",
+		"Number of patients in ICU with COVID-19": 870
+	}, {
+		"Reported Date": "2020-04-09T00:00:00",
+		"Number of patients in ICU with COVID-19": 880
+	}, {
+		"Reported Date": "2020-04-10T00:00:00",
+		"Number of patients in ICU with COVID-19": 920
+	}, {
+		"Reported Date": "2020-04-11T00:00:00",
+		"Number of patients in ICU with COVID-19": 935
+	}, {
+		"Reported Date": "2020-04-12T00:00:00",
+		"Number of patients in ICU with COVID-19": 950
+	}, {
+		"Reported Date": "2020-04-13T00:00:00",
+		"Number of patients in ICU with COVID-19": 1035
+	}, {
+		"Reported Date": "2020-04-14T00:00:00",
+		"Number of patients in ICU with COVID-19": 1110
+	}, {
+		"Reported Date": "2020-04-15T00:00:00",
+		"Number of patients in ICU with COVID-19": 1155
+	}, {
+		"Reported Date": "2020-04-16T00:00:00",
+		"Number of patients in ICU with COVID-19": 1180
+	}, {
+		"Reported Date": "2020-04-17T00:00:00",
+		"Number of patients in ICU with COVID-19": 1220
+	}, {
+		"Reported Date": "2020-04-18T00:00:00",
+		"Number of patients in ICU with COVID-19": 1225
+	}, {
+		"Reported Date": "2020-04-19T00:00:00",
+		"Number of patients in ICU with COVID-19": 1215
+	}, {
+		"Reported Date": "2020-04-20T00:00:00",
+		"Number of patients in ICU with COVID-19": 1215
+	}, {
+		"Reported Date": "2020-04-21T00:00:00",
+		"Number of patients in ICU with COVID-19": 1210
+	}, {
+		"Reported Date": "2020-04-22T00:00:00",
+		"Number of patients in ICU with COVID-19": 1190
+	}, {
+		"Reported Date": "2020-04-23T00:00:00",
+		"Number of patients in ICU with COVID-19": 1170
+	}, {
+		"Reported Date": "2020-04-24T00:00:00",
+		"Number of patients in ICU with COVID-19": 1120
+	}, {
+		"Reported Date": "2020-04-25T00:00:00",
+		"Number of patients in ICU with COVID-19": 1115
+	}, {
+		"Reported Date": "2020-04-26T00:00:00",
+		"Number of patients in ICU with COVID-19": 1090
+	}, {
+		"Reported Date": "2020-04-27T00:00:00",
+		"Number of patients in ICU with COVID-19": 1060
+	}, {
+		"Reported Date": "2020-04-28T00:00:00",
+		"Number of patients in ICU with COVID-19": 1000
+	}, {
+		"Reported Date": "2020-04-29T00:00:00",
+		"Number of patients in ICU with COVID-19": 970
+	}, {
+		"Reported Date": "2020-04-30T00:00:00",
+		"Number of patients in ICU with COVID-19": 900
+	}],
+	worst: [{
+		"Reported Date": "2020-03-19T00:00:00",
+		"Number of patients in ICU with COVID-19": 0
+	}, {
+		"Reported Date": "2020-03-20T00:00:00",
+		"Number of patients in ICU with COVID-19": 0
+	}, {
+		"Reported Date": "2020-03-21T00:00:00",
+		"Number of patients in ICU with COVID-19": 0
+	}, {
+		"Reported Date": "2020-03-22T00:00:00",
+		"Number of patients in ICU with COVID-19": 5
+	}, {
+		"Reported Date": "2020-03-23T00:00:00",
+		"Number of patients in ICU with COVID-19": 15
+	}, {
+		"Reported Date": "2020-03-24T00:00:00",
+		"Number of patients in ICU with COVID-19": 28
+	}, {
+		"Reported Date": "2020-03-25T00:00:00",
+		"Number of patients in ICU with COVID-19": 60
+	}, {
+		"Reported Date": "2020-03-26T00:00:00",
+		"Number of patients in ICU with COVID-19": 58
+	}, {
+		"Reported Date": "2020-03-27T00:00:00",
+		"Number of patients in ICU with COVID-19": 80
+	}, {
+		"Reported Date": "2020-03-28T00:00:00",
+		"Number of patients in ICU with COVID-19": 100
+	}, {
+		"Reported Date": "2020-03-29T00:00:00",
+		"Number of patients in ICU with COVID-19": 130
+	}, {
+		"Reported Date": "2020-03-30T00:00:00",
+		"Number of patients in ICU with COVID-19": 178
+	}, {
+		"Reported Date": "2020-03-31T00:00:00",
+		"Number of patients in ICU with COVID-19": 220
+	}, {
+		"Reported Date": "2020-04-01T00:00:00",
+		"Number of patients in ICU with COVID-19": 275
+	}, {
+		"Reported Date": "2020-04-02T00:00:00",
+		"Number of patients in ICU with COVID-19": 330
+	}, {
+		"Reported Date": "2020-04-03T00:00:00",
+		"Number of patients in ICU with COVID-19": 380
+	}, {
+		"Reported Date": "2020-04-04T00:00:00",
+		"Number of patients in ICU with COVID-19": 450
+	}, {
+		"Reported Date": "2020-04-05T00:00:00",
+		"Number of patients in ICU with COVID-19": 540
+	}, {
+		"Reported Date": "2020-04-06T00:00:00",
+		"Number of patients in ICU with COVID-19": 655
+	}, {
+		"Reported Date": "2020-04-07T00:00:00",
+		"Number of patients in ICU with COVID-19": 715
+	}, {
+		"Reported Date": "2020-04-08T00:00:00",
+		"Number of patients in ICU with COVID-19": 760
+	}, {
+		"Reported Date": "2020-04-09T00:00:00",
+		"Number of patients in ICU with COVID-19": 850
+	}, {
+		"Reported Date": "2020-04-10T00:00:00",
+		"Number of patients in ICU with COVID-19": 960
+	}, {
+		"Reported Date": "2020-04-11T00:00:00",
+		"Number of patients in ICU with COVID-19": 1050
+	}, {
+		"Reported Date": "2020-04-12T00:00:00",
+		"Number of patients in ICU with COVID-19": 1145
+	}, {
+		"Reported Date": "2020-04-13T00:00:00",
+		"Number of patients in ICU with COVID-19": 1265
+	}, {
+		"Reported Date": "2020-04-14T00:00:00",
+		"Number of patients in ICU with COVID-19": 1380
+	}, {
+		"Reported Date": "2020-04-15T00:00:00",
+		"Number of patients in ICU with COVID-19": 1440
+	}, {
+		"Reported Date": "2020-04-16T00:00:00",
+		"Number of patients in ICU with COVID-19": 1570
+	}, {
+		"Reported Date": "2020-04-17T00:00:00",
+		"Number of patients in ICU with COVID-19": 1680
+	}, {
+		"Reported Date": "2020-04-18T00:00:00",
+		"Number of patients in ICU with COVID-19": 1790
+	}, {
+		"Reported Date": "2020-04-19T00:00:00",
+		"Number of patients in ICU with COVID-19": 1915
+	}, {
+		"Reported Date": "2020-04-20T00:00:00",
+		"Number of patients in ICU with COVID-19": 2100
+	}, {
+		"Reported Date": "2020-04-21T00:00:00",
+		"Number of patients in ICU with COVID-19": 2280
+	}, {
+		"Reported Date": "2020-04-22T00:00:00",
+		"Number of patients in ICU with COVID-19": 2375
+	}, {
+		"Reported Date": "2020-04-23T00:00:00",
+		"Number of patients in ICU with COVID-19": 2535
+	}, {
+		"Reported Date": "2020-04-24T00:00:00",
+		"Number of patients in ICU with COVID-19": 2680
+	}, {
+		"Reported Date": "2020-04-25T00:00:00",
+		"Number of patients in ICU with COVID-19": 2770
+	}, {
+		"Reported Date": "2020-04-26T00:00:00",
+		"Number of patients in ICU with COVID-19": 2910
+	}, {
+		"Reported Date": "2020-04-27T00:00:00",
+		"Number of patients in ICU with COVID-19": 3000
+	}, {
+		"Reported Date": "2020-04-28T00:00:00",
+		"Number of patients in ICU with COVID-19": 3150
+	}, {
+		"Reported Date": "2020-04-29T00:00:00",
+		"Number of patients in ICU with COVID-19": 3265
+	}, {
+		"Reported Date": "2020-04-30T00:00:00",
+		"Number of patients in ICU with COVID-19": 3540
+	}]
+}
